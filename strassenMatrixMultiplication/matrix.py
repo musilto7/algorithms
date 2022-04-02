6a881f71722e00768497723a7ddf0e5779e328cd
Tomáš Musílek <tomasmusilek91@gmail.com>
2022-04-02 18:14:03 +0200

    Finish Strassen matrix Multiplication

--- strassenMatrixMultiplication/matrix.py
+++ strassenMatrixMultiplication/matrix.py
@@ -2,7 +2,45 @@ class Matrix:
     def __init__(self, array):
         self.array = array
 
+    def multiply(self, matrix):
+        if self._rows() == 1:
+            value = self.array[0][0] * matrix.array[0][0]
+            return Matrix([[value]])
+        (a11, a12, a21, a22) = self.quarter()
+        (b11, b12, b21, b22) = matrix.quarter()
+        s1 = b12.subtract(b22)
+        s2 = a11.add(a12)
+        s3 = a21.add(a22)
+        s4 = b21.subtract(b11)
+        s5 = a11.add(a22)
+        s6 = b11.add(b22)
+        s7 = a12.subtract(a22)
+        s8 = b21.add(b22)
+        s9 = a11.subtract(a21)
+        s10 = b11.add(b12)
+
+        p1 = a11.multiply(s1)
+        p2 = s2.multiply(b22)
+        p3 = s3.multiply(b11)
+        p4 = a22.multiply(s4)
+        p5 = s5.multiply(s6)
+        p6 = s7.multiply(s8)
+        p7 = s9.multiply(s10)
+
+        c11 = p5.add(p4).subtract(p2).add(p6)
+        c12 = p1.add(p2)
+        c21 = p3.add(p4)
+        c22 = p5.add(p1).subtract(p3).subtract(p7)
+
+        return _join_quarters(c11, c12, c21, c22)
+
     def add(self, matrix):
+        return self._add(matrix, 1)
+
+    def subtract(self, matrix):
+        return self._add(matrix, -1)
+
+    def _add(self, matrix, multiplier):
         rows = self._rows()
         columns = self._columns()
         new_array = [[0 for i in range(columns)] for _ in range(rows)]
@@ -10,7 +48,7 @@ class Matrix:
             for c in range(columns):
                 first = (self.array[r][c])
                 second = (matrix.array[r][c])
-                new_array[r][c] = first + second
+                new_array[r][c] = first + (second * multiplier)
         return Matrix(new_array)
 
     def quarter(self):
--- strassenMatrixMultiplication/test_matrix.py
+++ strassenMatrixMultiplication/test_matrix.py
@@ -25,6 +25,11 @@ def test_add_unit_matrix():
     matrix2 = Matrix([[2]])
     assert [[3]] == matrix1.add(matrix2).array
 
+def test_substract_unit_matrix():
+    matrix1 = Matrix([[1]])
+    matrix2 = Matrix([[2]])
+    assert [[-1]] == matrix1.subtract(matrix2).array
+
 
 def test_quarter_small_matrix_raises_exception():
     matrix = Matrix([[1]])
@@ -34,11 +39,11 @@ def test_quarter_small_matrix_raises_exception():
 
 def test_quarter_matrix():
     matrix = Matrix([[0, 1], [2, 3]])
-    quarters = matrix.quarter()
-    assert quarters[0].array == [[0]]
-    assert quarters[1].array == [[1]]
-    assert quarters[2].array == [[2]]
-    assert quarters[3].array == [[3]]
+    (a11, a12, a21, a22) = matrix.quarter()
+    assert a11.array == [[0]]
+    assert a12.array == [[1]]
+    assert a21.array == [[2]]
+    assert a22.array == [[3]]
 
 
 def test_join_quarters():
@@ -47,3 +52,15 @@ def test_join_quarters():
     matrix3 = Matrix([[3]])
     matrix4 = Matrix([[4]])
     assert [[1, 2], [3, 4]] == _join_quarters(matrix1, matrix2, matrix3, matrix4).array
+
+def test_multiply_trivial_matrix():
+    matrix1 = Matrix([[2]])
+    matrix2 = Matrix([[2]])
+    assert matrix1.multiply(matrix2).array == [[4]]
+
+def test_multiply_non_trivial_matrix():
+    matrix1 = Matrix([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
+
+    matrix2 = Matrix([[2, 0, 0, 0], [0, 2, 0, 0], [0, 0, 2, 0], [0, 0, 0, 2]])
+    resultMatrix = matrix1.multiply(matrix2)
+    assert resultMatrix.array == [[2, 4, 6, 8], [10, 12, 14, 16], [18, 20, 22, 24], [26, 28, 30, 32]]
